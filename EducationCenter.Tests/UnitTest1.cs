f92fe2b86a1b69ff8f985161e5ae6fc8f708b381
JuliaRebelles05 <juliarebelles@gmail.com>
2025-11-21 12:04:28 -0300

    Corrigindo a criação dos testes

---
+++ EducationCenter.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace EducationCenter.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- EducationCenter/Tests/EducationCenter.tests/StudentsControllerTests.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace EducationCenter.Tests.EducationCenter.tests
-{
-    public class StudentsControllerTests
-    {
-    }
-}
